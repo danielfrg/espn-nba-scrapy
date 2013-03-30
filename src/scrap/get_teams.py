bb5389b9c881a6964ea8578ce54358bff106acf7
Daniel Rodriguez <df.rodriguez143@gmail.com>
2013-03-29 23:34:29 -0500

    move files to src

---
+++ src/scrap/get_teams.py
@@ -0,0 +1,33 @@
+import copper
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+copper.project.path = '..'
+
+url = 'http://espn.go.com/nba/teams'
+r = requests.get(url)
+
+soup = BeautifulSoup(r.text)
+tables = soup.find_all('ul', class_='medium-logos')
+
+teams = []
+prefix_1 = []
+prefix_2 = []
+teams_urls = []
+for table in tables:
+    lis = table.find_all('li')
+    for li in lis:
+        info = li.h5.a
+        teams.append(info.text)
+        url = info['href']
+        teams_urls.append(url)
+        prefix_1.append(url.split('/')[-2])
+        prefix_2.append(url.split('/')[-1])
+
+
+dic = {'url': teams_urls, 'prefix_2': prefix_2, 'prefix_1': prefix_1}
+teams = pd.DataFrame(dic, index=teams)
+teams.index.name = 'team'
+print(teams)
+copper.save(teams, 'teams')
+
